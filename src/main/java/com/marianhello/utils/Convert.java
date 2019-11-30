dd5e309629e3836be29bb06c3045d3dd79118fcd
Mario Koch <mario@glazy.io>
2019-11-30 11:16:36 +0100

    Update, removed unneeded files

--- src/main/java/com/marianhello/utils/Convert.java
+++ src/main/java/com/marianhello/utils/Convert.java
@@ -20,7 +20,7 @@ public class Convert {
   public static int safeLongToInt(long l) {
     if (l < Integer.MIN_VALUE || l > Integer.MAX_VALUE) {
       throw new IllegalArgumentException
-        (l + " cannot be cast to int without changing its value.");
+              (l + " cannot be cast to int without changing its value.");
     }
     return (int) l;
   }
